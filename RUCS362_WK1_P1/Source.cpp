e5c1d7b37813a732e9bba691a57be7cc931f31ea
2alpha <andrae3000@gmail.com>
2015-09-04 22:03:52 -0400

    submitted as is

--- RUCS362_WK1_P1/Source.cpp
+++ RUCS362_WK1_P1/Source.cpp
@@ -1,5 +1,23 @@
-// RUCS362_WK1_P1.cpp : Defines the entry point for the console application.
-//
+
+//***************************************************************************
+//  FILENAME:	 AllenAssn1.cpp
+//  DESCRIPTION: This program will read, compute and display the results of 
+//               a text file containing Medical Plan type and expenses
+//  DATE:        9/1/15
+//  CLASS/TERM:  CS362_X40_Data Structures - CU_CS362 - XIN_X40_15F8W1
+//  DESIGNER:	 Andrae Allen
+//  FUNCTIONS:	 main - reads values, performs calculations, & displays results
+//               ProgramDescription - Displays a brief discription of the program
+//               readDataFile1 - reads data from txt file MEDICAL.txt data includes plan
+//				 type and expenses
+//				 calcArrayAverage - Calculates the numerical average of an array 
+//				 highestNumInArray - Calculates the highest number in an array
+//				 howManyAboveAverage - Calculates how many numbers are above the average
+//				 EmpOnlySummary- sumarizes the plan results for employees with employee 
+//				 only coverage
+//				 famPlanSummary - sumarizes the plan results for employees with family coverage
+//               displaysectionBreak - Creates a section break (space) on the console  
+//***************************************************************************
 
 #include <iostream> 
 #include <fstream>
@@ -12,22 +30,19 @@ void ProgramDescription();
 void readDataFile1(float[], float[], int&, int&);
 float calcArrayAverage(float[], int sizeOfArray);
 float highestNumInArray(float[], int sizeOfArray);
-int howManyAboveAverage(float[], int sizeOfArray, int arrayAverage);
+int howManyAboveAverage(float[], int sizeOfArray, float arrayAverage);
 
 void EmpOnlySummary(float[], int sizeOfArray);
 void famPlanSummary(float[], int sizeOfArray);
 void displaySectionBreak();
 
 
-const int MAX_ENTRIES = 4;
+const int MAX_ENTRIES = 500; // Max entries for either plan
 
-const string FILE_1_NAME = "MEDICAL.txt"; 
+const string FILE_1_NAME = "MEDICAL.txt"; // File name for input file
 
 int main()
 {
-
-	bool arrayFull = false;
-
 	float employeeOnlyList[MAX_ENTRIES];
 	float familyPlanList[MAX_ENTRIES];
 	int EmpOnlyCounter;
@@ -35,20 +50,21 @@ int main()
 
 	ProgramDescription();
 
-
 	readDataFile1(employeeOnlyList, familyPlanList, EmpOnlyCounter, famPlanCounter);
 	EmpOnlySummary(employeeOnlyList, EmpOnlyCounter);
 	displaySectionBreak();
 	famPlanSummary(familyPlanList, famPlanCounter);
-	
-	
-
-
-
 
 	system("PAUSE");
 	return 0;
 }
+
+//*************************************************************************
+//  FUNCTION:	  ProgramDescription
+//  DESCRIPTION:  Displays a brief discription of the program
+//  INPUT:        Parameters:  None
+//  OUTPUT: 	  Return value: None
+//*************************************************************************
 
 void ProgramDescription()
 {
@@ -61,20 +77,28 @@ void ProgramDescription()
 	cout << " 6. The highest expenses for Employees with Family Health Coverage" << endl;
 }
 
+//*************************************************************************
+//  FUNCTION:	  readDataFile1
+//  DESCRIPTION:  reads data from txt file MEDICAL.txt data includes plan
+//				  type and expenses
+//  INPUT:        Parameters:  empOnlyPlan - Array for holding emploee only plan expenses 
+//                             famPlan - Array for holding family plan expenses 
+//							   EmpOnlyCounter - counts number of employees with employee only plan
+//							   famPlanCounter - counts number of employees with family plan
+//  OUTPUT: 	  Return value: None
+//*************************************************************************
 
 void readDataFile1(float empOnlyPlan[], float famPlan[], int& EmpOnlyCounter, int& famPlanCounter)
 {
-	ifstream inputFile1;
-	char planType;
+	ifstream inputFile1;       //file stream variable
+	char planType;             // plan type 
 
-	bool oArrayFull = false;
-	bool fArrayFull = false;
-
-	EmpOnlyCounter = 0;
-	famPlanCounter = 0;
+	EmpOnlyCounter = 0;        // employee only plan counter
+	famPlanCounter = 0;        // family plan counter
 
 	inputFile1.open(FILE_1_NAME);
 
+	// try to read specified data file 
 	if (inputFile1.fail())
 	{
 		cout << endl;
@@ -84,6 +108,7 @@ void readDataFile1(float empOnlyPlan[], float famPlan[], int& EmpOnlyCounter, in
 		exit(5);
 	}
 
+	// If success opening specified file show success message and continue 
 	else
 	{
 		cout << endl;
@@ -91,47 +116,36 @@ void readDataFile1(float empOnlyPlan[], float famPlan[], int& EmpOnlyCounter, in
 		cout << endl;
 	}
 
+	// read until end of file
+	while (inputFile1)       
+	{
+		inputFile1 >> planType; 
 
-	while (inputFile1)       // successful read (data on line)
-	{                                       // and array not full on last pass
-
-
-		inputFile1 >> planType;  // read age into array
-
+		//If Employee Only Plan Type  
 		if (planType == 'O' && EmpOnlyCounter < MAX_ENTRIES)
-		{
-
-			//cout << "Single" << endl;
+		{		
 			inputFile1 >> empOnlyPlan[EmpOnlyCounter];
 			EmpOnlyCounter++;
 			inputFile1.ignore(100, '\n');
 
 			if (EmpOnlyCounter >= MAX_ENTRIES)
-			{
-				oArrayFull = true;
+			{;
 				cout << "Alert! Maximum number of Entries (" << MAX_ENTRIES << ") for Employee Only Plan has been reached" << endl << endl;;
 			}
-
-
 		}
 
+		//If Family Plan type
 		else if (planType == 'F' && famPlanCounter < MAX_ENTRIES)
-		{
-
-			
+		{			
 			inputFile1 >> famPlan[famPlanCounter];
 			famPlanCounter++;
 			inputFile1.ignore(100, '\n');
 
 			if (famPlanCounter >= MAX_ENTRIES)
 			{
-				fArrayFull = true;
 				cout << "Alert! Maximum number of Entries (" << MAX_ENTRIES << ") for Family Plan has been reached" << endl << endl;
 			}
-
 		}
-
-
 	}
 
 	inputFile1.close();
@@ -139,13 +153,24 @@ void readDataFile1(float empOnlyPlan[], float famPlan[], int& EmpOnlyCounter, in
 
 }
 
+//*************************************************************************
+//  FUNCTION:	  EmpOnlySummary
+//  DESCRIPTION:  sumarizes the plan results for employees with employee 
+//				  only coverage
+//  INPUT:        Parameters:  empOnlyPlan - employee only plan array
+//                             EmpOnlyCounter - counts number of employees with employee only plan
+//  CALLS TO:	  calcArrayAverage, highestNumInArray, howManyAboveAverage
+//  OUTPUT: 	  Return value: None
+//*************************************************************************
+
 void EmpOnlySummary(float empOnlyPlan[], int EmpOnlyCounter)
 {
-	float empOnlyPlanAverage,
-		empOnlyplanHgh;
+	float empOnlyPlanAverage,    // Employee only plan average
+		  empOnlyplanHgh;		 // Highest number in employee only plan array
 	
-	int empAboveAverage = 0;
+	int empAboveAverage = 0;     // How many employee are above the average expense
 
+	// If number of employees with employee only plan is greater than zero 
 	if (EmpOnlyCounter > 0){
 		cout << "For " << EmpOnlyCounter << " employees with employee only coverage:" << endl;
 
@@ -159,18 +184,28 @@ void EmpOnlySummary(float empOnlyPlan[], int EmpOnlyCounter)
 		cout << "      " << empAboveAverage << " Employee(s) expenses were above the average" << endl;
 	}
 
+	// If number of employee with employee only plan is zero
 	else cout << "There were NO employees with employee Only coverage." << endl;
 }
 
 
+//*************************************************************************
+//  FUNCTION:	  famPlanSummar
+//  DESCRIPTION:  Sumarizes the plan results for employees with family coverage
+//  INPUT:        Parameters:  famPlan - famaily plan array
+//                             famPlanCounter - counts number of employees with family plan
+//  CALLS TO:	  calcArrayAverage, highestNumInArray, howManyAboveAverage
+//  OUTPUT: 	  Return value: None
+//*************************************************************************
 
 void famPlanSummary(float famPlan[], int famPlanCounter)
 {
-	float famPlanAverage,
-		  famPlanHgh;
+	float famPlanAverage,     // family plan average
+		  famPlanHgh;         // family plan high 
 
-	int famAboveAverage = 0;
+	int famAboveAverage = 0;  // How many employees are above the average expense
 
+	// If number of employees with family plan is greater than zero
 	if (famPlanCounter > 0){
 		cout << "For " << famPlanCounter << " employees with family coverage:" << endl;
 
@@ -184,15 +219,24 @@ void famPlanSummary(float famPlan[], int famPlanCounter)
 		cout << "      " << famAboveAverage << " Employee(s) expenses were above the average" << endl;
 	}
 
+	// If number of employee with family plan is zero
 	else cout << "There were NO employees with family Coverage." << endl;
 
 
 }
 
+//*************************************************************************
+//  FUNCTION:	  calcArrayAverage
+//  DESCRIPTION:  Calculates the numerical average of an array
+//  INPUT:        Parameters:  ArrayName - the name of the array being averaged
+//                             sizeOfArray - size of the array being averaged 
+//  OUTPUT: 	  Return value: averageOfArray - average of the array 
+//*************************************************************************
+
 float calcArrayAverage(float ArrayName[], int sizeOfArray)
 {
-	float sumOfArray = 0,
-	      averageOfArray = 0; 
+	float sumOfArray = 0,       // sum of the array
+	      averageOfArray = 0;	// average of the array 
 	
 
 	for (int index = 0; index < sizeOfArray; index++)
@@ -205,9 +249,17 @@ float calcArrayAverage(float ArrayName[], int sizeOfArray)
 	return averageOfArray;
 }
 
+//*************************************************************************
+//  FUNCTION:	  highestNumInArray
+//  DESCRIPTION:  Calculates the highest number in an array
+//  INPUT:        Parameters:  ArrayName - the name of the array being averaged
+//                             sizeOfArray - size of the array being averaged 
+//  OUTPUT: 	  Return value: highestValue- the highest value in the array
+//*************************************************************************
+
 float highestNumInArray(float ArrayName[], int sizeOfArray)
 {
-	float highestValue = 0;
+	float highestValue = 0; // the highest value in the array
 
 	for (int index = 0; index < sizeOfArray; index++)
 	{
@@ -218,9 +270,18 @@ float highestNumInArray(float ArrayName[], int sizeOfArray)
 
 }
 
-int howManyAboveAverage(float ArrayName[], int sizeOfArray, int arrayAverage)
+//*************************************************************************
+//  FUNCTION:	  Calculates how many numbers are above the average
+//  DESCRIPTION:  Computes value of a base number raised to an exponent
+//  INPUT:        Parameters:  ArrayName - the name of host array 
+//                             sizeOfArray - size of the host 
+//							   average - average of specified array
+//  OUTPUT: 	  Return value: numAboveAverage - how many numbers are above the average
+//*************************************************************************
+
+int howManyAboveAverage(float ArrayName[], int sizeOfArray, float arrayAverage)
 {
-	int numAboveAverage = 0; 
+	int numAboveAverage = 0; // how many numbers are above the average
 
 	for (int index = 0; index < sizeOfArray; index++)
 	{
@@ -233,6 +294,13 @@ int howManyAboveAverage(float ArrayName[], int sizeOfArray, int arrayAverage)
 
 	return numAboveAverage;
 }
+
+//*************************************************************************
+//  FUNCTION:	  displaySectionBreak
+//  DESCRIPTION:  Creates a section break (space) on the console
+//  INPUT:        Parameters:  None
+//  OUTPUT: 	  Return value: None
+//*************************************************************************
 
 void displaySectionBreak()
 {
